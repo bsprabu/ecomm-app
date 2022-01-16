06b6d250e5e9da495f722a156760a6760c178ed0
prabu <bspdevalways@gmail.com>
2022-01-17 05:12:01 +0530

    ---e2e testing changes

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -6,17 +6,40 @@ describe('Page Load  Test', () => {
   })
 })
 
+describe('Range Slider', () => {
+  it('range slider minimum value', () => {     
+    cy.wait(1000);
+    cy.get('.ngx-slider-pointer-min').click({ multiple: true, force: true }).type('{rightarrow}{rightarrow}');
+    cy.wait(1000);
+    cy.get('.ngx-slider-pointer-max').click({ multiple: true, force: true }).type('{leftarrow}{leftarrow}')
+  })  
+})
+
 describe('RAM filter checkbox', () => {
-  it('check the input', () => {
-    cy.visit('/')
-    cy.get(':nth-child(5) > .form-check-input').click({force:true});
-    cy.contains('Server List')
+  it('check the checkbox', () => {
+    cy.get(':nth-child(5) > .form-check-input').click({force:true});    
+    cy.wait(1000);
   })
-  // it('unscheck the input', () => {
-  //   cy.visit('/')
-  //   cy.get(':nth-child(5) > .form-check-input').uncheck({force:true});
-  //   cy.contains('Server List')
-  // })
+  it('uncheck the checkbox', () => {
+    cy.get(':nth-child(5) > .form-check-input').uncheck({force:true});
+    cy.wait(1000);
+  })
+  it('check the checkbox', () => {
+    cy.get(':nth-child(10) > .form-check-input').click({force:true});    
+    cy.wait(1000);
+  })
+  it('uncheck the checkbox', () => {
+    cy.get(':nth-child(10) > .form-check-input').uncheck({force:true});
+    cy.wait(1000);
+  })
+})
+
+describe('HDD dropdown', () => {
+  it('select the hard disk type', () => {
+    cy.get('.form-select').select(1);
+    cy.wait(1000);
+    cy.get('.form-select').select(2);
+  })
 })
 
 
